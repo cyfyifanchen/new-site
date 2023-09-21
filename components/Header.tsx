5d52a297a1f278dcc0b7615983fcd73c971ceb9f
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-09-21 10:39:41 +0800

    tweaking

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Logo from '@/data/master.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -12,15 +12,20 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">{/* <Logo /> */}</div>
+            <div className="mr-2 pt-10 md:pt-3">
+              <Logo />
+            </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div className="mb-1 h-6 text-4xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
               siteMetadata.headerTitle
             )}
           </div>
+          <p className="text-sm w-5text-center text-gray-400 mt-10 md:mt-2 w-100">
+            favorite language CSS ¯\_(ツ)_/¯
+          </p>
         </Link>
       </div>
       <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
